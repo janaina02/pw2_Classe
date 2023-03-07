465c6dacc0e1ea54c36a050fd567b7c201f99377
LeonardoSP <LeonardoSP>
2023-03-07 08:22:57 -0300

    Commit Inicial

--- java/pw2_objetos/src/Anime.java
+++ java/pw2_objetos/src/Anime.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Anime {
+import java.util.Date;
+
+public class Anime {
+    public int id;
+    public String nome;
+    public Date dtLancamento;
+    public int classificacao;
+    public String genero;
+    public String autor;
+    public int qtdEpisodios;
+    public int temporadas;
 }
--- java/pw2_objetos/src/AnimeTeste.java
+++ java/pw2_objetos/src/AnimeTeste.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class AnimeTeste {
+public class AnimeTeste {
+    public static void main(String[] args) {
+
+    }
 }
